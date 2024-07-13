adf5dfc0c253bb9b7592a7552c8b26c279c4a023
Fabricio Orrala <fzzio007@gmail.com>
2024-07-13 10:23:50 -0500

    Fix apollo vars

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: process.env.MATIAS_API_URL,
+  uri: process.env.EXPO_PUBLIC_MATIAS_API_URL,
 });
 
 const client = new ApolloClient({
