33aca919cff9ac2eca714445973324a434aff695
Fabricio Orrala <fzzio007@gmail.com>
2024-09-03 14:55:40 -0500

    Reporte template

--- app/reports/total.tsx
+++ app/reports/total.tsx
@@ -1,38 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { Text, Surface, Button } from 'react-native-paper';
-import { useSacraments } from '@/hooks/useSacraments';
-import { useLocations } from '@/hooks/useLocations';
-import { useCatechismLevels } from '@/hooks/useCatechismLevels';
-import { useCatechumens } from '@/hooks/useCatechumens';
 import { commonStyles, buttonStyles } from '@/styles';
-import { Catechumen } from '@/types';
 import { useRouter } from 'expo-router';
-
-interface ChartDataItem {
-  name: string;
-  amount: number;
-  total: number;
-}
+import { useSurveys } from '@/hooks/useSurveys';
 
 export default function TotalReports() {
   const router = useRouter();
-  const { catechumens, catechumensTotal } = useCatechumens();
-  const { locations } = useLocations();
-  const { sacraments } = useSacraments();
-  const { loading, error, catechismLevels } = useCatechismLevels();
+  const { surveys } = useSurveys();
 
-  if (loading) return <Text style={commonStyles.loadingText}>Cargando...</Text>;
-  if (error) return <Text style={commonStyles.errorText}>Error: {error.message}</Text>;
+
+  const downloadAllSurveysAsExcel = () => {
+    // TODO
+  }
 
 
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
       <Surface style={commonStyles.surface}>
         <View style={commonStyles.headerTitle}>
-          <Text style={commonStyles.title}>Reportes por Curso</Text>
+          <Text style={commonStyles.title}>Reportes Totales</Text>
         </View>
         <View style={styles.body}>
+          <Button
+            mode="outlined"
+            onPress={() => downloadAllSurveysAsExcel}
+            style={buttonStyles.primaryButton}
+            labelStyle={buttonStyles.primaryButtonLabel}
+          >
+            Descargar Excel
+          </Button>
+        </View>
+        <View style={styles.footer}>
           <Button
             mode="outlined"
             onPress={() => router.back()}
@@ -53,6 +52,10 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   body: {
+    gap: 16,
+    marginBottom: 5
+  },
+  footer: {
     gap: 16,
   },
 });
